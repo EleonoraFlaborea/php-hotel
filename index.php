7d1454c8330ebeadabd32993b9da0b0527c1ad5e
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-02-20 18:13:56 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,79 @@
+<?php
+
+//require_once __DIR__ . '/data.inc';
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotels</title>
+     <!--Bootstrap-->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+</head>
+<body>
+
+<section class="container pt-5">
+    <h1>HOTEL LIST</h1>
+
+
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">name</th>
+      <th scope="col">description</th>
+      <th scope="col">parking</th>
+      <th scope="col">vote</th>
+      <th scope="col">distance_to_center</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Hotel Belvedere</th>
+      <td>Hotel Belvedere Descrizione</td>
+      <td>Otto</td>
+      <td>4</td>
+      <td>10.4</td>
+    </tr>
+    <tr>
+      <td>2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+      <td>10.4</td>
+    </tr>
+    <tr>
+      <td>3</th>
+      <td>Larry the Bird</td>
+      <td>@twitter</td>
+      <td>10.4</td>
+      <td>10.4</td>
+    </tr>
+    <tr>
+      <td>2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+      <td>10.4</td>
+    </tr>
+    <tr>
+      <td>3</th>
+      <td>Larry the Bird</td>
+      <td>@twitter</td>
+      <td>10.4</td>
+      <td>10.4</td>
+    </tr>
+  </tbody>
+</table>
+
+</section>
+
+    
+</body>
+</html>
