70038de620b1046991c110216599145c08063b7d
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-02-20 21:25:29 +0100

    add table

--- index.php
+++ index.php
@@ -1,10 +1,47 @@
 <?php
 
 //require_once __DIR__ . '/data.inc';
+$hotels = [
 
+  [
+      'name' => 'Hotel Belvedere',
+      'description' => 'Hotel Belvedere Descrizione',
+      'parking' => true,
+      'vote' => 4,
+      'distance_to_center' => 10.4
+  ],
+  [
+      'name' => 'Hotel Futuro',
+      'description' => 'Hotel Futuro Descrizione',
+      'parking' => true,
+      'vote' => 2,
+      'distance_to_center' => 2
+  ],
+  [
+      'name' => 'Hotel Rivamare',
+      'description' => 'Hotel Rivamare Descrizione',
+      'parking' => false,
+      'vote' => 1,
+      'distance_to_center' => 1
+  ],
+  [
+      'name' => 'Hotel Bellavista',
+      'description' => 'Hotel Bellavista Descrizione',
+      'parking' => false,
+      'vote' => 5,
+      'distance_to_center' => 5.5
+  ],
+  [
+      'name' => 'Hotel Milano',
+      'description' => 'Hotel Milano Descrizione',
+      'parking' => true,
+      'vote' => 2,
+      'distance_to_center' => 50
+  ],
+
+];
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -21,57 +58,25 @@
 <section class="container pt-5">
     <h1>HOTEL LIST</h1>
 
+<?php
 
-    <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">name</th>
-      <th scope="col">description</th>
-      <th scope="col">parking</th>
-      <th scope="col">vote</th>
-      <th scope="col">distance_to_center</th>
+$htmlTable = '<table border="1">';
+foreach ($hotels as $row) {
+    $htmlTable .= '<tr>';
+    foreach ($row as $cell) {
+        $htmlTable .= '<td>' . $cell . '</td>';
+    }
+    $htmlTable .= '</tr>';
+}
+$htmlTable .= '</table>';
 
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>Hotel Belvedere</th>
-      <td>Hotel Belvedere Descrizione</td>
-      <td>Otto</td>
-      <td>4</td>
-      <td>10.4</td>
-    </tr>
-    <tr>
-      <td>2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>10.4</td>
-    </tr>
-    <tr>
-      <td>3</th>
-      <td>Larry the Bird</td>
-      <td>@twitter</td>
-      <td>10.4</td>
-      <td>10.4</td>
-    </tr>
-    <tr>
-      <td>2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>10.4</td>
-    </tr>
-    <tr>
-      <td>3</th>
-      <td>Larry the Bird</td>
-      <td>@twitter</td>
-      <td>10.4</td>
-      <td>10.4</td>
-    </tr>
-  </tbody>
-</table>
+echo $htmlTable;
+?>
 
+</section>
+
+<section>
+  
 </section>
 
     
